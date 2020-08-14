05d03891ff38fb2a4a6f6de236df22fc8e1a6683
Pavlos Khetab <pavlos.khetab@gmail.com>
2020-08-14 11:38:09 +0100

    Added Final Question with errors as py file.

---
+++ TestFinalQuestionErrors.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: ")
+op = str(input("enter operator (+, -, *): "))
+if op == "+"
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "*:
+    result  num1 * num2
+print(str(num1) + " " + op + " " + str(num2) + " = " + str(result))
